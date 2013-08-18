cab7f0167bb82b823e56b2b3dc13d4623f6c3bb5
Kruithne <kruithne@gmail.com>
2013-08-18 17:08:58 +0100

    Allow data checking.

--- src/no/runsafe/survivalchallenge/objectives/CraftItemObjective.java
+++ src/no/runsafe/survivalchallenge/objectives/CraftItemObjective.java
@@ -3,6 +3,7 @@ package no.runsafe.survivalchallenge.objectives;
 import no.runsafe.framework.api.event.inventory.ICraftItem;
 import no.runsafe.framework.minecraft.Item;
 import no.runsafe.framework.minecraft.event.inventory.RunsafeCraftItemEvent;
+import no.runsafe.framework.minecraft.item.meta.RunsafeMeta;
 import no.runsafe.framework.minecraft.player.RunsafePlayer;
 import no.runsafe.survivalchallenge.BaseObjective;
 import no.runsafe.survivalchallenge.ObjectiveHandler;
@@ -18,9 +19,14 @@ public abstract class CraftItemObjective extends BaseObjective implements ICraft
 	public void OnCraftItem(RunsafeCraftItemEvent event)
 	{
 		RunsafePlayer player = event.getWhoClicked();
-		if (handler.entityInEligibleWorld(player) && event.getCurrentItem().is(item))
-			this.award(player);
+		if (handler.entityInEligibleWorld(player))
+		{
+			RunsafeMeta item = event.getCurrentItem();
+			if ((item.is(this.item) && data == -1 || item.getDurability() == data))
+				this.award(player);
+		}
 	}
 
 	protected Item item;
+	protected short data = -1;
 }
